37fe41920067fb6b2047435b7f9a2986b60151ee
js988174 <76925402+js988174@users.noreply.github.com>
2022-10-08 13:13:21 +0900

    [Bronze V] Title: ??!, Time: 204 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    
+    String list = "??!";
+    String s = scanner.next();
+    
+    System.out.print(s + list);
+    }
+}
